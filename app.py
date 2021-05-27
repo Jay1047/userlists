8742e7b6ceca48f18108fa64f03959213a57b4b7
Your Name <you@example.com>
2021-05-27 14:28:45 +0900

    First commit Server listen

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+    
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    return '<h1>Hello World!<h1>'
+
+if __name__ =='__main__':
+    app.run(port=5000)
